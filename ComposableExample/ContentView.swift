551126e3c9ae757721fcf3aee90c83e409e00030
Bryan Bolivar <bryan.andres.bolivar@gmail.com>
2020-07-31 16:04:09 -0500

    Initial Commit

---
+++ ComposableExample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ComposableExample
+//
+//  Created by Bryan Bolivar on 31/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
